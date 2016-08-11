878b8b29bb95b2292a76d0bd9abe3dec1be248da
parrt <parrt@cs.usfca.edu>
2016-08-10 18:07:14 -0700

    add linked

---
+++ notes/code/linkedin/test.py
@@ -0,0 +1,39 @@
+import sys
+from linkedin import linkedin
+import BaseHTTPServer
+import urlparse
+
+KEY = sys.argv[1]
+SECRET = sys.argv[2]
+
+token = None
+
+def _wait_for_user_to_enter_browser(app):
+    class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+        def do_GET(self):
+            global token
+            p = self.path.split('?')
+            if len(p) > 1:
+                params = urlparse.parse_qs(p[1], True, True)
+                app.authentication.authorization_code = params['code'][0]
+                app.authentication.state = params['state'][0]
+                token = app.authentication.get_access_token()
+
+    server_address = ('', 8000)
+    httpd = BaseHTTPServer.HTTPServer(server_address, MyHandler)
+    httpd.handle_request()
+    return token
+
+# KEY = "wFNJekVpDCJtRPFX812pQsJee-gt0zO4X5XmG6wcfSOSlLocxodAXNMbl0_hw3Vl"
+# SECRET = "daJDa6_8UcnGMw1yuq9TjoO_PMKukXMo8vEMo7Qv5J-G3SPgrAV0FqFCd0TNjQyG"
+RETURN_URL = "http://localhost:8000/"
+authentication = linkedin.LinkedInAuthentication(KEY, SECRET, RETURN_URL,
+                                                 ['r_basicprofile'])
+print authentication.authorization_url
+application = linkedin.LinkedInApplication(authentication)
+
+_wait_for_user_to_enter_browser(application)
+print token # set in do_GET
+
+print application.get_connections()
+print application.get_profile()
---
+++ notes/linkedin.md
@@ -0,0 +1,10 @@
+# Pulling data from LinkedIn
+
+You must create a known entity (application) that will communicate with the API. Start by reading [OAuth 2.0 for linkedin](https://developer.linkedin.com/docs/oauth2)
+
+[Create an application](https://www.linkedin.com/secure/developer?newapp=)
+
+i just used my name:
+
+<img src="figures/linkedin-app-settings.png" width=280>
+
