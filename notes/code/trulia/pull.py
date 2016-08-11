71cca68c1efc0e4e2ce84c42104921a42482ecdc
parrt <parrt@cs.usfca.edu>
2016-08-11 13:25:37 -0700

    add xml

--- notes/code/trulia/pull.py
+++ notes/code/trulia/pull.py
@@ -5,7 +5,10 @@ import urllib
 import urllib2
 import sys
 import json
-import xml.etree.ElementTree as ET
+
+# https://untangle.readthedocs.io/en/latest/
+# https://github.com/stchris/untangle
+import untangle
 
 KEY=sys.argv[1]
 
@@ -17,8 +20,24 @@ state = "CA"
 URL = LocationInfoURL % (state, KEY)
 print URL
 response = urllib2.urlopen(URL)
-jsondata = response.read()              # read all data
+xmldata = response.read()              # read all data
+# print xmldata
 
-data = json.loads(jsondata)             # dictionary version
+testxml = """<?xml version="1.0"?>
+<TruliaWebServices>
+    <response>
+        <LocationInfo>
+            <city>
+                <cityId>86930</cityId>
+                <name>Yountville</name>
+                <longitude>-122.367593491246</longitude>
+                <latitude>38.3938834972556</latitude>
+            </city>
+        </LocationInfo>
+    </response>
+</TruliaWebServices>
+"""
 
-print json.dumps(data, indent=4)
+xml = untangle.parse(xmldata)
+for city in xml.TruliaWebServices.response.LocationInfo.city:
+    print city.cityId.cdata, city.name.cdata, city.longitude.cdata, city.latitude.cdata
--- notes/xml.md
+++ notes/xml.md
@@ -1,3 +1,27 @@
 # XML
 
 http://eli.thegreenplace.net/2012/03/15/processing-xml-in-python-with-elementtree/
+
+The simplest way to handle XML is with [untangle lib](https://github.com/stchris/untangle); see [untangle doc](https://untangle.readthedocs.io/en/latest/).
+
+```python
+import untangle
+testxml = """<?xml version="1.0"?>
+<TruliaWebServices>
+    <response>
+        <LocationInfo>
+            <city>
+                <cityId>86930</cityId>
+                <name>Yountville</name>
+                <longitude>-122.367593491246</longitude>
+                <latitude>38.3938834972556</latitude>
+            </city>
+        </LocationInfo>
+    </response>
+</TruliaWebServices>
+"""
+
+xml = untangle.parse(testxml)
+for city in xml.TruliaWebServices.response.LocationInfo.city:
+    print city.cityId.cdata, city.name.cdata, city.longitude.cdata, city.latitude.cdata
+```
