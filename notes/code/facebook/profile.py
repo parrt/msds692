961a68216886f9098fab29298439ca14a0a52ba6
parrt <parrt@cs.usfca.edu>
2016-08-12 14:09:39 -0700

    got it to pull my name out.

--- notes/code/facebook/profile.py
+++ notes/code/facebook/profile.py
@@ -9,35 +9,73 @@
 import sys
 import BaseHTTPServer
 import urlparse
+import urllib
 import urllib2
+import json
 import webbrowser
 
 APP_CODE = None
+APP_ACCESS_TOKEN = None
 
 # Do $ python -m SimpleHTTPServer or do this:
 def _wait_for_user_to_enter_browser():
     class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         def do_GET(self):
-            global APP_CODE
+            global APP_CODE, APP_ACCESS_TOKEN
             p = self.path.split('?')
             if len(p) > 1:
                 params = urlparse.parse_qs(p[1], True, True)
-                APP_CODE = params['code'][0]
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write("Heh, you logged into Facebook!")
+                if p[0]=='/login':
+                    APP_CODE = params['code'][0]
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write("You logged in!")
+                elif p[0]=='/exchange':
+                    APP_ACCESS_TOKEN = params['access_token'][0]
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write("Access token obtained!")
+
 
     server_address = ('', 8000)
     httpd = BaseHTTPServer.HTTPServer(server_address, MyHandler)
     httpd.handle_request()
 
-KEY = sys.argv[1]
+APP_ID = sys.argv[1]
+APP_SECRET = sys.argv[2]
 
-RETURN_URL = "http://localhost:8000/"
-
-URL = "https://www.facebook.com/dialog/oauth?%%20client_id=%s&redirect_uri=http://localhost:8000/"
-webbrowser.open_new_tab(URL % KEY)
+LOGIN_URL = "https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=http://localhost:8000/login"
+webbrowser.open_new_tab(LOGIN_URL % APP_ID)
 
 _wait_for_user_to_enter_browser()
 
-print "App code is", APP_CODE
+# print "App code is", APP_CODE
+
+EXCH_URL = "https://graph.facebook.com/v2.3/oauth/access_token" \
+           "?client_id=%s" \
+           "&redirect_uri=http://localhost:8000/login" \
+           "&client_secret=%s" \
+           "&code=%s"
+URL = EXCH_URL % (APP_ID, urllib.quote(APP_SECRET), urllib.quote(APP_CODE))
+# print URL
+#webbrowser.open_new_tab(URL)
+
+response = urllib2.urlopen(URL)
+jsondata = response.read()
+json_dict = json.loads(jsondata)
+ACCESS_TOKEN = json_dict['access_token']
+# print ACCESS_TOKEN
+
+# Ok, now we can pull data
+# https://developers.facebook.com/docs/graph-api/using-graph-api/
+
+PROFILE_URL = "https://graph.facebook.com/me" \
+              "?access_token=%s"
+
+PROFILE_URL = PROFILE_URL % ACCESS_TOKEN
+# print PROFILE_URL
+
+response = urllib2.urlopen(PROFILE_URL)
+jsondata = response.read()
+json_dict = json.loads(jsondata)
+print json_dict['name']
--- notes/rest.m
+++
@@ -1,5 +0,0 @@
-# REST APIs
-
-[Huge source of public APIs](https://www.publicapis.com/)
-
-[Open payments REST API sample query](http://openpayments.us/data?query=John+Chan)
