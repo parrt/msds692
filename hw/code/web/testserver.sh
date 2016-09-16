8cdb11788601f464ae17e8b173ce3bbabe8f3cc9
parrt <parrt@cs.usfca.edu>
2016-09-16 15:20:53 -0700

    tweak test routine

--- hw/code/web/testserver.sh
+++ hw/code/web/testserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTPUT=$1
+USER=$1
 MYSRC=~/courses/msan692-private/hw/pipeline
 
 for t in TSLA VBK
@@ -8,14 +8,17 @@ do
 	echo "Testing" $t
 
 	# Test GET
-	curl -s http://parrt.pythonanywhere.com/history/$t > /tmp/$t.html
-        python $MYSRC/htmlcompare.py $OUTPUT/$t.html /tmp/$t.html
+	curl -s http://$USER.pythonanywhere.com/history/$t > /tmp/$t.html
+	curl -s http://parrt.pythonanywhere.com/history/$t > /tmp/parrt-$t.html
+        python $MYSRC/htmlcompare.py /tmp/$t.html /tmp/parrt-$t.html
 
 	# Test POST
-	curl -s --data "ticker=$t" http://parrt.pythonanywhere.com/history > /tmp/post-$t.html
-        python $MYSRC/htmlcompare.py $OUTPUT/$t.html /tmp/post-$t.html
+	curl -s --data "ticker=$t" http://$USER.pythonanywhere.com/history > /tmp/post-$t.html
+	curl -s --data "ticker=$t" http://parrt.pythonanywhere.com/history > /tmp/parrt-post-$t.html
+        python $MYSRC/htmlcompare.py /tmp/post-$t.html /tmp/parrt-post-$t.html
 
 	# Test GET JSON
-	curl -s http://parrt.pythonanywhere.com/data/$t > /tmp/$t.json
-        python $MYSRC/jsoncompare.py $OUTPUT/$t.json /tmp/$t.json
+	curl -s http://$USER.pythonanywhere.com/data/$t > /tmp/$t.json
+	curl -s http://parrt.pythonanywhere.com/data/$t > /tmp/parrt-$t.json
+        python $MYSRC/jsoncompare.py /tmp/$t.json /tmp/parrt-$t.json
 done
