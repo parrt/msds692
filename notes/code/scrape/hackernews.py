2dfd914a94d6f897c532563c97eb559693291fff
parrt <parrt@cs.usfca.edu>
2016-09-24 14:10:33 -0700

    add hacker news scarf

---
+++ notes/code/scrape/hackernews.py
@@ -0,0 +1,9 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+response = urllib2.urlopen("https://news.ycombinator.com/newest")
+html = BeautifulSoup(response, "html.parser")
+
+
+for link in html.find_all(class_="storylink"):
+    print link['href'], link.text
