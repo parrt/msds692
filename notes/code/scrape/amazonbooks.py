79d3732e5cfd7ac0bb6fad75cd8a741770449d9d
Terence Parr <parrt@antlr.org>
2017-04-29 15:47:17 -0700

    improved amazon version

--- notes/code/scrape/amazonbooks.py
+++ notes/code/scrape/amazonbooks.py
@@ -1,28 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-def parseAmazonBestSellers():
-    response = requests.get("http://www.amazon.com/gp/bestsellers/books/ref=sv_b_2",
-                          params={'User-Agent': "Resistance is futile"})
-    soup = BeautifulSoup(response.text, "html.parser")
+URL = "https://www.amazon.com/gp/bestsellers/books/ref=sv_b_2"
 
-    books = []
-    for item in soup.find_all(class_="zg_itemWrapper"):
-        link = item.find(class_="a-link-normal")
-        priceitem = item.find(class_="a-size-base a-color-price")
-        if priceitem is None: continue
-        price = priceitem.string.strip()
-        href = link['href'].strip()
-        title = link.img['alt'].strip()
-        auth = item.find(class_="a-size-small a-link-child")
-        if auth: # some missing an author?
-            author = auth.text.strip()
-            books.append((price, title, author, href))
+r = requests.get(URL)
+htmltext = r.text
 
-    return books
+soup = BeautifulSoup(htmltext, 'html.parser')
 
-books = parseAmazonBestSellers()
-for price, title, author, href in books:
-    print title, author, price
-    print href
-    print
+bestsellers = []
+for item in soup.find_all(class_="zg_itemWrapper"):
+    link = item.div.a
+    img = link.div.img
+    authtag = item.find(class_="a-size-small a-link-child")
+    pricetag = item.find(class_="a-size-base a-color-price")
+    price = pricetag.text
+    ratingtag = item.find(class_="a-icon-alt")
+    rating = ratingtag.text # 4.7 out of 5 stars
+    words = rating.split(' ')
+    rating = words[0]
+    if authtag:
+        auth = authtag.text
+    else:
+        auth = "unknown"
+    info = [auth, img['alt'], price, rating, link['href']]
+    bestsellers.append(info)
+
+# ok, now write to a csv file in excel format
+f = open("/tmp/bestsellers.csv", "w")
+fw = csv.writer(f, dialect='excel')
+
+fw.writerow(['author', 'title', 'price', 'rating', 'link'])
+for book in bestsellers:
+    fw.writerow(book)
+
+f.close()
