42a56cbab18d7b1302c3b7cffac78d765d5c161c
Terence Parr <parrt@antlr.org>
2017-04-29 12:27:57 -0700

    tweak

--- notes/code/selenium/google_search.py
+++ notes/code/selenium/google_search.py
@@ -1,7 +1,9 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, if not specified will search path.
+# driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, if not specified will search path.
+# or
+driver = webdriver.Chrome('/anaconda/chromedriver-Darwin')
 driver.get('http://www.google.com')
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('USF Analytics')
