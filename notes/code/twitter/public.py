9e0d2ca23840d2ede234b87b7ed9bf6c8fdd0026
parrt <parrt@cs.usfca.edu>
2017-08-15 10:42:14 -0700

    add twitter code

---
+++ notes/code/twitter/public.py
@@ -0,0 +1,28 @@
+# Pull in parrt's public tweets using REST API at twitter
+# It appears Twitter's Search API searches recent tweets from the past 7-9 days and the
+# search is rate limited to 180 queries per 15 minute window.
+
+import tweepy
+
+def loadkeys(filename):
+    """"
+    load parrt's keys/tokens from CSV file with form
+    consumer_key, consumer_secret, access_token, access_token_secret
+    """
+    with open(filename) as f:
+        items = f.readline().strip().split(', ')
+        return items
+
+
+consumer_key, consumer_secret, \
+access_token, access_token_secret \
+    = loadkeys("/Users/parrt/Dropbox/licenses/twitter.csv")
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text
---
+++ notes/code/twitter/trump_tweets.py
@@ -0,0 +1,27 @@
+import tweepy
+
+
+def loadkeys(filename):
+    """"
+    load parrt's keys/tokens from CSV file with form
+    consumer_key, consumer_secret, access_token, access_token_secret
+    """
+    with open(filename) as f:
+        items = f.readline().strip().split(', ')
+        return items
+
+
+consumer_key, consumer_secret, \
+access_token, access_token_secret \
+    = loadkeys("/Users/parrt/Dropbox/licenses/twitter.csv")
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+user = api.get_user('realDonaldTrump')
+print "followers", user.followers_count
+
+for status in tweepy.Cursor(api.user_timeline, id='realDonaldTrump').items():
+    print status
