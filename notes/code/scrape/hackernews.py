59156d4d83003f464192a4dfd3e6525bb742f8e3
Terence Parr <parrt@antlr.org>
2017-10-04 10:39:26 -0700

    tweak

--- notes/code/scrape/hackernews.py
+++ notes/code/scrape/hackernews.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get("https://news.ycombinator.com/newest")
-html = BeautifulSoup(response.content, "html.parser")
+html = BeautifulSoup(response.text, "html.parser")
 
-for link in html.find_all(class_="storylink"):
-    print link['href'], link.text
+# for link in html.find_all(class_="storylink"):
+for link in html.find_all("a", {"class":"storylink"}):
+        print link['href'], link.text
