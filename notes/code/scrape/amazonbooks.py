7b55ed1bd4914e111851189b6e60306bd1f9d3d6
parrt <parrt@cs.usfca.edu>
2018-09-30 14:22:23 -0700

    Update notes

--- notes/code/scrape/amazonbooks.py
+++ notes/code/scrape/amazonbooks.py
@@ -2,29 +2,49 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-URL = "https://www.amazon.com/gp/bestsellers/books/ref=sv_b_2"
-
+URL = "https://www.amazon.com/gp/bestsellers/books"
 r = requests.get(URL)
 htmltext = r.text
 
-soup = BeautifulSoup(htmltext, 'html.parser')
+""" Test with this:
+with open("/tmp/t.html") as f:
+    htmltext = f.read()
+"""
+
+htmltext = htmltext.strip()
+soup = BeautifulSoup(htmltext, 'lxml') # 'html.parser' doesn't work here!
 
 bestsellers = []
-for item in soup.find_all(class_="zg_itemWrapper"):
-    link = item.div.a
+for item in soup.find_all('span', {'class':"aok-inline-block zg-item"}):
+    # Link
+    link = item.a
     img = link.div.img
-    authtag = item.find(class_="a-size-small a-link-child")
-    pricetag = item.find(class_="a-size-base a-color-price")
-    price = pricetag.text
-    ratingtag = item.find(class_="a-icon-alt")
-    rating = ratingtag.text # 4.7 out of 5 stars
-    words = rating.split(' ')
-    rating = words[0]
+
+    # Author
+    'Author like this: <span class ="a-size-small a-color-base" >Craig Smith</span>'
+    'OR <a class="a-size-small a-link-child" ...>Bob Woodward</a>'
+    authtag = item.find(class_="a-size-small a-color-base")
+    if not authtag:
+        authtag = item.find(class_="a-size-small a-link-child")
     if authtag:
         auth = authtag.text
     else:
         auth = "unknown"
-    info = [auth, img['alt'], price, rating, link['href']]
+
+    # Price
+    pricetag = item.find(class_="a-size-base a-color-price")
+    price = pricetag.text
+
+    # Rating
+    ratingtag = item.find(class_="a-icon-alt")
+    rating = None
+    if ratingtag:
+        rating = ratingtag.text # 4.7 out of 5 stars
+        words = rating.split(' ')
+        rating = words[0]
+
+    # Pack together as tuple
+    info = (auth, img['alt'], price, rating, link['href'])
     bestsellers.append(info)
 
 # ok, now write to a csv file in excel format
