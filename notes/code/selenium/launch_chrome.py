48538ad88b60c6867864bdac1715fa5cacced3a6
parrt <parrt@cs.usfca.edu>
2020-12-01 10:30:39 -0800

    tweak notes

--- notes/code/selenium/launch_chrome.py
+++ notes/code/selenium/launch_chrome.py
@@ -2,10 +2,12 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, if not specified will search path.
-#driver = webdriver.Chrome('/Users/parrt/anaconda2/chromedriver-Darwin')  # Optional argument, if not specified will search path.
+# or, fill in your ANACONDA_INSTALL_DIR and try this:
+# driver = webdriver.Chrome('ANACONDA_INSTALL_DIR/chromedriver-Darwin')
+
 driver.get('http://www.google.com')
 
+# here is where some useful work would typically happen
+
 input("Press Enter to quit")
-# OR:
-# time.sleep(3) # let the user see something
-driver.quit()
+driver.quit() # close browser
