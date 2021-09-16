9ef4c67f61d7db6d4eb5aab6ba50a515dd9b0f9d
parrt <parrt@cs.usfca.edu>
2021-09-16 09:45:14 -0700

    update notes

--- hw/code/recommender/doc2vec.py
+++ hw/code/recommender/doc2vec.py
@@ -109,8 +109,8 @@ def load_articles(articles_dirname, gloves):
 
     We use gloves parameter to compute the word vectors and centroid.
 
-    The filename is stripped of the prefix of the articles_dirname pulled in as
-    script parameter sys.argv[2]. E.g., filename will be "business/223.txt"
+    The filename is fully-qualified name of the text file including
+    the path to the root of the corpus passed in on the command line.
     """
 
 
