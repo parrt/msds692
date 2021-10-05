4ef920fb7ca1ae60ad5fa0ec9158c088036fff96
Terence Parr <parrt@antlr.org>
2021-10-05 09:58:09 -0700

    update samples

--- hw/code/sentiment/pulltwitter.sh
+++ hw/code/sentiment/pulltwitter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl "http://"$(cat IP.txt)"/the_antlr_guy" > parrt-tweets.html
-curl "http://"$(cat IP.txt)"/following/the_antlr_guy" > parrt-following.html
-curl "http://"$(cat IP.txt)"/realdonaldtrump" > trump-tweets.html
-curl "http://"$(cat IP.txt)"/following/realdonaldtrump" > trump-following.html
+IP=$(cat IP.txt)
+curl "http://"$IP"/the_antlr_guy" > parrt-tweets.html
+curl "http://"$IP"/following/the_antlr_guy" > parrt-following.html
+curl "http://"$IP"/joebiden" > joebiden-tweets.html
+curl "http://"$IP"/following/joebiden" > joebiden-following.html
